2c9961a033602f5f877f88540e04417fcd068ac2
Michael Bailey <m-bailey@gmx.com>
2022-02-09 08:31:24 +0000

    initial pre-call screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function CallSetup() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="call-setup-overlay">
+      <div className="call-setup-modal">
+        <div className="modal-top">
+          <div className="modal-header">
+            <h3 className="title">Beitrittsbestätigung</h3>
+            <span className="subtitle">Möchten Sie diesem Gespräch beitreten?</span>
+          </div>
+          <button className="modal-close">X</button>
+        </div>
+        <VideoFrame />
+        <div className="video-setup-dropdowns">
+          <Dropdown title="Gewähltes Kamera"/>
+          <Dropdown title="Gewähltes Mikrofon"/>
+          <Dropdown title="Gewählte Lautsprecher"/>
+        </div>
+        <a className="video-setup-reset">Geräte neu auswählen</a>
+        <button className="video-setup-confirm">Gespräch beitreten</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+function VideoFrame() {
+  return (
+    <div className="video-container">
+      <img src={logo} className="video-frame" alt="video" />
+      <div className="video-controls">
+          <button className="signal-button">Signal Gut <span className="signal-update">Aktualisieren</span></button>
+          <button className="push-right circle-button">M</button>
+          <button className="circle-button">C</button>
+      </div>
+    </div>
+  );
+}
+
+function Dropdown({title, options}) {
+  return (
+    <div className="">
+      <label for="webcam-select">{title}</label>
+      <select name="webcam-select">
+        <option value="123">FaceTime HD camera</option>
+      </select>
+    </div>
+  );
+}
+
+export default CallSetup;
