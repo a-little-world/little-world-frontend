9ca7966969e1b661ed3e6c452a237ff006e28af6
tim <tim@timschupp.de>
2022-03-29 19:53:27 +0200

    added base url env var

--- ENVIRONMENT.js
+++ ENVIRONMENT.js
@@ -1,2 +1,4 @@
 exports.DEFAULT_LOGIN_USERNAME = "benjamin.tim@gmx.de"
 exports.DEFAULT_LOGIN_PASSWORD = "Test123"
+/* Neat since, we only have to set BACKEND_URL="" in production */
+exports.BACKEND_URL = "littleworld-test.com"
