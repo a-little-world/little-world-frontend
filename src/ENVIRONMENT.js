fb3f8eb5741816917eb54df2d72810cbaea0662a
tim <tim@timschupp.de>
2022-03-29 22:30:18 +0200

    set test url for ENV

--- src/ENVIRONMENT.js
+++ src/ENVIRONMENT.js
@@ -1,4 +1,5 @@
 exports.DEFAULT_LOGIN_USERNAME = "benjamin.tim@gmx.de"
 exports.DEFAULT_LOGIN_PASSWORD = "Test123"
 /* Neat since, we only have to set BACKEND_URL="" in production */
-exports.BACKEND_URL = "littleworld-test.com"
+exports.BACKEND_URL = "https://littleworld-test.com"
+// exports.BACKEND_URL = "http://localhost:8000"
