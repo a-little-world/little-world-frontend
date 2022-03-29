246372c7abecd7d099c8982a6674b1f6562cd9ec
tim <tim@timschupp.de>
2022-03-30 01:42:10 +0200

    modified credentials, updated cookie

--- src/login-simulator.js
+++ src/login-simulator.js
@@ -17,6 +17,8 @@ export async function simulatedAutoLogin(username, password, by_force=false){
   const login_data = await $.ajax({
     type: "POST",
     url: `${GLOB.BACKEND_URL}/api2/login_hack/`,
+    // headers: {  'Access-Control-Allow-Origin': '*' },
+    crossDomain:true,
     /* 
      * This uses the `login_hack` api, which is an adaptation of the regular login
      * - also returns `csrftoken` this is usualy set when loading the page under the default backend
@@ -29,10 +31,10 @@ export async function simulatedAutoLogin(username, password, by_force=false){
   })
 
   // patching the cookie, this would also usually not be needed cause it would be set if loaded via regular backend
-  document.cookie = `csrftoken=${login_data.csrfcookie}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`;
+  document.cookie = `csrftoken=${login_data.csrfcookie}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`
 
   // The session login cookie should be set automaticly
-  document.cookie = `sessionid=${login_data.sessionid}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`;
+  document.cookie = `sessionid=${login_data.sessionid}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`
 
   window.localStorage.setItem("login_data", login_data)
   window.localStorage.setItem("user_loggedin", true)
@@ -43,8 +45,17 @@ export async function simulatedAutoLogin(username, password, by_force=false){
   if we set `xhrFields: { withCredentials: true }` It does it anyways.
   */
 
+  
   $.ajaxSetup({ /* This will set this for all future calls */
-    xhrFields: { withCredentials: true }
+    xhrFields: { withCredentials: true },
+    /*
+    * So yeah mikes idea was the best all along. 
+    * Somehow there is no way to teach the browser to send the `sessionid` to another domain.
+    * So we just store the authentication and use 'Basic ...'
+    */
+    headers : {
+        Authorization: `Basic ${btoa(username + ":" + password)}`,
+    }
   })
 
   return login_data
