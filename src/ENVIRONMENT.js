1d1b2fa691964ac9ce9a55a8d97dbd9e98ea4204
tim <tim@timschupp.de>
2022-04-22 14:07:01 +0200

    added serveral other development flags, made it local and production 'ready'

--- src/ENVIRONMENT.js
+++ src/ENVIRONMENT.js
@@ -1,7 +1,10 @@
-exports.DEFAULT_LOGIN_USERNAME = "benjamin.tim@gmx.de";
-exports.DEFAULT_LOGIN_PASSWORD = "Test123";
-// exports.BACKEND_URL = "https://littleworld-test.com" -> for production
-// This is the url of the 'docker-nginx-proxy' container created via 'schrodingers-nginx.sh'
-exports.BACKEND_URL = ""; //"http://localhost:81";
-exports.BACKEND_PATH = "/frontend";
 exports.DEVELOPMENT = false;
+// Yeah I know this is somewhat redundant, but there is a difference between backend-dev and frontend-dev 
+// e.g.: we need to use 'wss' for websocket in production but 'ws' in both frontend and backend localdev
+exports.PRODUCTION = false; 
+exports.DEFAULT_LOGIN_USERNAME = this.DEVELOPMENT ? "benjamin.tim@gmx.de" : "nopeHeAintExistInProduction:)";
+exports.DEFAULT_LOGIN_PASSWORD = this.DEVELOPMENT ? "Test123" : "aPassYouCantUse:)";
+// exports.BACKEND_URL = "" -> for production
+// This is the default url of the 'docker-nginx-proxy' container created via 'schrodingers-nginx.sh' -> "http://localhost:81";
+exports.BACKEND_URL = this.DEVELOPMENT ? "http://localhost:81" : "";
+exports.BACKEND_PATH = this.DEVELOPMENT ? "" : "/app";
