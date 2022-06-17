c14fd81974f919f71b2f96d656bf81269eff742b
Michael Bailey <m6b@gmx.com>
2022-06-17 18:33:33 +0100

    kill removeActiveTracks call in link-prepend; runs on create not click

--- src/path-prepend.jsx
+++ src/path-prepend.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { BACKEND_PATH } from "./ENVIRONMENT";
-import { removeActiveTracks } from "./twilio-helper";
 
 function Link(props) {
   const { to } = props;
-
-  // kill video if linked out of call
-  if (!["/call-setup", "/call"].includes(to)) {
-    removeActiveTracks();
-  }
   return <RouterLink {...props} to={`${BACKEND_PATH}${to}`} />; // eslint-disable-line react/jsx-props-no-spreading
 }
 
