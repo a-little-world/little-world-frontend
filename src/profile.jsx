a8ff1022cf87f53e76ad9911a6a8b5e9ad88e069
Michael Bailey <m6b@gmx.com>
2022-06-19 17:33:49 +0100

    (m) remove unused imports on profile page

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import "./i18n";
 import Link from "./path-prepend";
