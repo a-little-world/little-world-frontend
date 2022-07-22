22dc8c3bc2abfc4839538fdf617917ca7b0d9d2d
tim <tim@timschupp.de>
2022-07-22 18:45:06 +0200

    links dont get disabled if they have a 'onClick' set

--- src/path-prepend.jsx
+++ src/path-prepend.jsx
@@ -4,8 +4,8 @@ import { Link as RouterLink } from "react-router-dom";
 import { BACKEND_PATH } from "./ENVIRONMENT";
 
 function Link(props) {
-  const { to, className } = props;
-  const newClasses = to ? className : `${className} disabled`;
+  const { to, onClick, className } = props;
+  const newClasses = to || onClick ? className : `${className} disabled`;
 
   return <RouterLink {...props} to={`${BACKEND_PATH}${to}`} className={newClasses} />; // eslint-disable-line react/jsx-props-no-spreading
 }
