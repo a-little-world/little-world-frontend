cb120f47356327ce5616efa141770ba19756c720
tim <tim@timschupp.de>
2022-09-09 19:35:35 +0200

    simuldated data

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import * as dataJSON from "./data.json";
 import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -396,63 +397,7 @@ function Main() {
   }, [location]);
 
   useEffect(() => {
-    $.ajax({
-      type: "POST",
-      url: `${BACKEND_URL}/api2/composite/`,
-      headers: {
-        // The cookies is optained when authenticating via `api2/login/`
-        "X-CSRFToken": Cookies.get("csrftoken"), // the login_had sets this, see 'login-simulator.js'
-      },
-      data: {
-        "composite-request": JSON.stringify([
-          {
-            spec: {
-              type: "simple",
-              ref: "_matchesBasic",
-            },
-            method: "GET",
-            path: "api2/matches/",
-          },
-          {
-            spec: {
-              type: "simple",
-              ref: "unconfirmedMatches",
-            },
-            method: "GET",
-            path: "api2/unconfirmed_matches/", // TODO @tbscode yes i'll compine the matches and unconfirmed matches api :)
-          },
-          {
-            spec: {
-              type: "simple",
-              ref: "userData",
-            },
-            method: ["GET", "OPTIONS"],
-            path: "api2/profile/",
-          },
-          {
-            spec: {
-              type: "simple",
-              ref: "userState", // State of the current user
-            },
-            method: ["GET", "OPTIONS"],
-            path: "api2/user_state/",
-          },
-          {
-            spec: {
-              type: "foreach",
-              in: "_matchesBasic",
-              as: "match",
-              ref: "matches",
-            },
-            method: "POST",
-            path: "api2/profile_of/",
-            body: {
-              partner_h256_pk: "${match.user_h256_pk}", // eslint-disable-line no-template-curly-in-string
-            },
-          },
-        ]),
-      },
-    }).then(
+    Promise.resolve(JSON.parse(JSON.stringify(dataJSON))).then(
       ({
         _matchesBasic,
         unconfirmedMatches,
