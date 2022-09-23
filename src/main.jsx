e6706d524c6d039a45f09584a44e71637f924da1
Michael Bailey <m6b@gmx.com>
2022-09-23 18:30:53 +0100

    link notifications sidebar button and main page notification panel

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ function Sidebar({ userInfo, sidebarMobile }) {
   const buttonData = [
     { label: "start", path: "/" },
     { label: "messages", path: "/chat" },
-    { label: "notifications", path: "" },
+    { label: "notifications", path: "/notifications" },
     { label: "my_profile", path: "/profile" },
     { label: "help", path: "" },
     { label: "settings", path: "/settings" },
@@ -343,7 +343,9 @@ function NotificationPanel({ userInfo }) {
           </div>
         ))}
       </div>
-      <Link className="show-all">{t("nbr_show_all")}</Link>
+      <Link to="/notifications" className="show-all">
+        {t("nbr_show_all")}
+      </Link>
     </div>
   );
 }
