0cd0e4129a223ddcb16dc2e92a9b092507543be1
Michael Bailey <m6b@gmx.com>
2022-09-23 18:29:10 +0100

    create initial notifications page / files

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import CallSetup, { IncomingCall } from "./call-setup";
 import Chat from "./chat/chat-full-view";
 import { BACKEND_PATH, BACKEND_URL } from "./ENVIRONMENT";
 import "./i18n";
+import Notifications from "./notifications";
 import Overlay from "./overlay";
 import Link from "./path-prepend";
 import Profile, { ProfileBox } from "./profile";
@@ -574,6 +575,7 @@ function Main({ initData }) {
           </div>
         )}
         {use === "chat" && initChatComponent}
+        {use === "notifications" && <Notifications />}
         {use === "profile" && (
           <Profile
             matchesInfo={matchesInfo}
