9dd9c519bf65aa2b4624cf429db550bc5e2000c2
Michael Bailey <m6b@gmx.com>
2022-09-23 18:48:23 +0100

    add visible notifications type toggle buttons, unstyled

--- src/notifications.jsx
+++ src/notifications.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import "./notifications.css";
 
 function Notifications() {
   const { t } = useTranslation();
+  const [visibleNotifs, setVisibleNotifs] = useState("all");
   return (
     <>
       <div className="header">
         <span className="text">{t("nm_main_header_settings")}</span>
+        <div className="buttons">
+          <button
+            type="button"
+            className={visibleNotifs === "all" ? "show-all selected" : "show-all"}
+            onClick={() => {
+              setVisibleNotifs("all");
+            }}
+          >
+            {t("nm_filter_all")}
+          </button>
+          <button
+            type="button"
+            className={visibleNotifs === "unread" ? "show-unread selected" : "show-unread"}
+            onClick={() => {
+              setVisibleNotifs("unread");
+            }}
+          >
+            {t("nm_filter_unread")}
+          </button>
+          <button
+            type="button"
+            className={visibleNotifs === "archive" ? "show-archive selected" : "show-archive"}
+            onClick={() => {
+              setVisibleNotifs("archive");
+            }}
+          >
+            {t("nm_filter_archive")}
+          </button>
+        </div>
       </div>
       <div className="content"></div>
     </>
