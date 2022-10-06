0d41427cee9b819c14501ebe00f032385e1f2dcd
Michael Bailey <m6b@gmx.com>
2022-10-06 22:05:07 +0100

    revise fundamental format and structure of modal

--- src/settings.jsx
+++ src/settings.jsx
@@ -15,19 +15,24 @@ function ListItem({ title, text, setEditing }) {
   );
 }
 
-function InputItem({ label, initVal, example, repeat }) {
-  const [value, setValue] = useState(initVal);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
+function ModalBox({ label, value, setEditing }) {
   return (
-    <>
-      <div className="label">{label}</div>
-      <input type="text" value={value} onChange={handleChange} placeholder={example} />
-      {repeat && <InputItem label={`repeat ${label}`} repeat={false} />}
-    </>
+    <div className="edit-modal">
+      <h2>change {label}</h2>
+      <button type="button" className="modal-close" onClick={() => setEditing(false)} />
+      <div className="input-container">
+        <label htmlFor={label}>{label}</label>
+        <input type="text" defaultValue={value} name={label} />
+      </div>
+      <div className="buttons">
+        <button type="button" className="save" onClick={() => setEditing(false)}>
+          save
+        </button>
+        <button type="button" className="cancel" onClick={() => setEditing(false)}>
+          cancel
+        </button>
+      </div>
+    </div>
   );
 }
 
@@ -35,57 +40,38 @@ function Settings() {
   const { t } = useTranslation();
   const [editing, setEditing] = useState(false);
 
-  const data = {
-    name: [
-      {
-        label: "First name",
-        example: "Max",
-        val: "John",
-        regex: "",
-      },
-      {
-        label: "Second name",
-        example: "Power",
-        val: "Smith",
-        regex: "",
-      },
-    ],
-    email: [
-      {
-        label: "email",
-        example: "max.power@gmail.com",
-        val: "j.smith69@gmx.de",
-        repeat: true,
-      },
-    ],
-    password: [
-      {
-        label: "password",
-        repeat: true,
-      },
-    ],
-    "phone number": [
-      {
-        label: "phone number",
-        example: "0519-345 95 704",
-        val: "0123-456-76-754",
-      },
-    ],
-    "post code": [
-      {
-        label: "post code",
-        example: "44986",
-        val: "90210",
-      },
-    ],
-    "birth year": [
-      {
-        label: "birth year",
-        example: "1962",
-        val: "1969",
-      },
-    ],
-  };
+  const data = [
+    {
+      label: "first name",
+      value: "John",
+    },
+    {
+      label: "last name",
+      value: "Smith",
+    },
+    {
+      label: "e-mail",
+      value: "j.smith69@gmx.de",
+      repeat: true,
+    },
+    {
+      label: "password",
+      value: "********",
+      repeat: true,
+    },
+    {
+      label: "phone number",
+      value: "0123-456-76-754",
+    },
+    {
+      label: "post code",
+      value: "90210",
+    },
+    {
+      label: "birth year",
+      value: "1969",
+    },
+  ];
 
   return (
     <>
@@ -96,9 +82,8 @@ function Settings() {
         <section className="settings personal">
           <h2>{t("sg_section_personal")}</h2>
           <div className="settings-items">
-            {Object.entries(data).map(([title, fields]) => {
-              const text = fields.map(({ val }) => val).join(" ");
-              return <ListItem key={title} title={title} text={text} setEditing={setEditing} />;
+            {data.map(({ label, value }) => {
+              return <ListItem key={label} title={label} text={value} setEditing={setEditing} />;
             })}
             <div className="item">
               <h3>Delete Account</h3>
@@ -108,22 +93,11 @@ function Settings() {
             </div>
             <div className={editing ? "edit-overlay" : "edit-overlay hidden"}>
               {editing && (
-                <div className="edit-modal">
-                  {data[editing].map(({ label, example, val, regex, repeat }) => (
-                    <InputItem
-                      key={label}
-                      label={label}
-                      example={example}
-                      initVal={val}
-                      regex={regex}
-                      repeat={repeat}
-                    />
-                  ))}
-                  <button type="button" onClick={() => setEditing(false)}>
-                    cancel
-                  </button>
-                  <button type="button">submit</button>
-                </div>
+                <ModalBox
+                  label={editing}
+                  value={data.filter(({ label }) => label === editing)[0].value}
+                  setEditing={setEditing}
+                />
               )}
             </div>
           </div>
