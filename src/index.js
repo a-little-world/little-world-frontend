af3a102410d292c0b3d844f7609730bffc9ad86e
tim <tim@timschupp.de>
2023-01-08 17:42:34 +0100

    updated login simulator and integrated new login simulator

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ import GLOB from "./ENVIRONMENT";
 import { updateTranslationResources } from "./i18n";
 import reportWebVitals from "./reportWebVitals";
 
+console.log("STARTUP");
+
 if (GLOB.DEVELOPMENT) {
   import("./loginSimulator.js").then((simulator) => {
-    simulator
-      .simulatedAutoLogin(GLOB.DEFAULT_LOGIN_USERNAME, GLOB.DEFAULT_LOGIN_PASSWORD)
-      .then((data) => {
-        console.log(data);
-        console.log("start rendering now...");
+    simulator.simulatedAutoLogin().then((data) => {
+      console.log(data);
+      console.log("start rendering now...");
 
-        const initData = data.initial_profile_data;
-        ReactDOM.render(
-          <React.StrictMode>
-            <App initData={initData} />
-          </React.StrictMode>,
-          document.getElementById("root")
-        );
+      const initData = data.initial_profile_data;
+      ReactDOM.render(
+        <React.StrictMode>
+          <App initData={initData} />
+        </React.StrictMode>,
+        document.getElementById("root")
+      );
 
-        reportWebVitals();
-      });
+      reportWebVitals();
+    });
   });
 } else {
   window.renderApp = ({ initData }, { apiTranslations }) => {
