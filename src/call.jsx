3c8137e8e9345e1dc28714a11216489633775f79
tim <tim@timschupp.de>
2023-03-14 15:17:21 +0100

    readded more translation languages

--- src/call.jsx
+++ src/call.jsx
@@ -276,7 +276,26 @@ function SidebarNotes() {
 
 function TranslationDropdown({ side, selected, setter }) {
   const { t } = useTranslation();
-  const languages = ["en", "de"];
+  const languages = [
+    "en",
+    "sa",
+    "bg",
+    "ma",
+    "ca",
+    "fr",
+    "gr",
+    "in",
+    "it",
+    "kr",
+    "ir",
+    "pl",
+    "ru",
+    "es",
+    "tr",
+    "ua",
+    "vn",
+    "de",
+  ];
   return (
     <select
       name={`${side}-language-select`}
