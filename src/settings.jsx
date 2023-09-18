d3fae307363e50af678998b82b0a61aa3cb58949
tim <tim@timschupp.de>
2023-09-18 10:24:39 +0200

    added delete account button and confirmation modal for it

--- src/settings.jsx
+++ src/settings.jsx
@@ -301,10 +301,41 @@ function ModalBox({ label, valueIn, setEditing }) {
   );
 }
 
+
+function ConfirmAccountDeletetion({ 
+  showModal,
+  setShowModal
+}) {
+  const { t } = useTranslation();
+
+  return ( showModal &&
+  <div className={showModal ? "overlay-shade" : "overlay-shade hidden"}>
+    <div className="modal-box">
+      <button type="button" className="modal-close" onClick={() => setShowModal(false)} />
+      <div className="content">
+        <div className="message-text">{t("settings_delete_account_confirm_title")}</div>
+        <div className="buttons">
+          <button type="button" className="confirm" onClick={() => {
+            // call deletion api ...
+            // then reload page ...
+          }}>
+            {t("settings_delete_account_confirm_button")}
+          </button>
+          <button type="button" className="cancel" onClick={() => setShowModal(false)}>
+            {t("settings_delete_account_confirm_cancel")}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
 function Settings() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const [editing, setEditing] = useState(null);
+  const [showConfirm, setShowConfirm] = useState(false);
 
   const items = [
     // with ordering
@@ -354,14 +385,15 @@ function Settings() {
                 />
               );
             })}
-            {/* No deleting of account for now!
             <div className="item">
               <h3>{t("sg_personal_delete_account")}</h3>
-              <button type="button" className="delete-account">
+              <button type="button" className="delete-account"
+                  onClick={() => {
+                    setShowConfirm(true);
+                  }}>
                 {t("sg_personal_delete_account_btn")}
               </button>
             </div>
-            */}
           </div>
         </section>
       </div>
@@ -375,6 +407,7 @@ function Settings() {
           />
         )}
       </div>
+      <ConfirmAccountDeletetion showModal={showConfirm} setShowModal={setShowConfirm}/>
     </>
   );
 }
