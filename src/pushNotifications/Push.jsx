1cc0505fd9931b5b886ef06344644bd3de68a8aa
Tim Schupp <tim@timschupp.de>
2024-10-30 13:59:26 +0100

    simple push-notifications experiment succeeded

--- src/pushNotifications/Push.jsx
+++ src/pushNotifications/Push.jsx
@@ -17,28 +17,80 @@ const PushDebugerContainer = styled.div`
   left: 0;
 `;
 
+export const PushDebuggerButton = styled(Button)`
+  font-size: 1rem;
+  font-weight: normal;
+  justify-content: flex-start;
+  padding: ${({ theme }) => theme.spacing.small};
+  width: fit-content;
+
+  &:not(:last-of-type) {
+    margin-bottom: ${({ theme }) => theme.spacing.xxsmall};
+  }
+`;
+
+
 function InteralPushDebugger(){
 
     const theme = useTheme();
-    return <><h1>Push Debugger</h1>
-            <Button
+    
+    const requestNotificationPermission = async () => {
+        if ('Notification' in window) {
+          try {
+            const permission = await Notification.requestPermission();
+            if (permission === 'granted') {
+              alert('Push notifications permission granted!');
+            } else {
+              alert('Push notifications permission denied.');
+            }
+          } catch (error) {
+            console.error('Error requesting notification permission:', error);
+          }
+        } else {
+          alert('This browser does not support desktop notification');
+        }
+    };
+    
+    const triggerTestNotification = () => {
+        if (Notification.permission === 'granted') {
+        console.log("Notification permission already granted, trying to trigger one!")
+          new Notification('Test Notification', {
+            body: 'This is a test push notification!',
+            icon: 'https://via.placeholder.com/48', // Example icon URL
+          });
+        } else {
+          alert('Please grant notification permissions first.');
+        }
+      };
+
+    return <PushDebugerContainer>
+            <h1>Push Debugger</h1>
+            <PushDebuggerButton
               type="button"
-              variation={ButtonVariations.Circle}
+              variation={ButtonVariations.Basic}
               appearance={ButtonAppearance.Primary}
               borderColor={theme.color.text.link}
               color={theme.color.text.link}
+              onClick={requestNotificationPermission}
             >
                 Request Push Permissions
-            </Button>
-            </>
+            </PushDebuggerButton>
+            <PushDebuggerButton
+              type="button"
+              variation={ButtonVariations.Basic}
+              appearance={ButtonAppearance.Primary}
+              borderColor={theme.color.text.link}
+            onClick={triggerTestNotification}
+              color={theme.color.text.link}>
+                Trigger Test Push Notification
+          </PushDebuggerButton>
+    </PushDebugerContainer>
 }
 
 export function CustomPushDebugger() {
     
     return <CustomPushDebuggerRoot >
-        <PushDebugerContainer>
             <InteralPushDebugger/>
-        </PushDebugerContainer>
     </CustomPushDebuggerRoot>
 }
 
