20b79c9b11a33988521f5c318c52252258cf19b4
Simba14 <seanblundell@gmail.com>
2024-11-01 20:12:23 +0100

    improve styling of preferences page

--- src/components/blocks/MailingLists/MailingLists.tsx
+++ src/components/blocks/MailingLists/MailingLists.tsx
@@ -1,24 +1,45 @@
-import { Switch } from '@a-little-world/little-world-design-system';
-import React, { useEffect } from 'react';
+import {
+  Button,
+  Loading,
+  MessageTypes,
+  StatusMessage,
+  Switch,
+} from '@a-little-world/little-world-design-system';
+import { LoadingSizes } from '@a-little-world/little-world-design-system/dist/esm/components/Loading/Loading';
+import React, { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-
-import useSWR from 'swr';
-import { mutateUserData } from '../../../api';
-import { updateProfile } from '../../../features/userData';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import useSWR from 'swr';
+
+import { mutateUserData } from '../../../api/index.js';
+import { updateProfile } from '../../../features/userData.js';
 import { onFormError } from '../../../helpers/form.ts';
 
-const MailingListsWrapper = styled.div``;
+const MailingListsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: ${({ theme }) => theme.spacing.xxsmall};
+  margin: ${({ theme }) => theme.spacing.small} auto 0;
+  width: 100%;
+  max-width: 400px;
+  min-height: 232px; // height of content
+`;
 
-export enum EmailCategories {
-  Community = 'community',
-  Marketing = 'marketing',
-  Newsletter = 'newsletter',
-  Partnerships = 'partnerships',
-}
+const CategoryForm = styled.form`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing.xxsmall};
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+`;
 
 const MailingLists = ({
   inline,
@@ -89,12 +110,9 @@ const MailingLists = ({
   );
 };
 
-const fetcher = url => fetch(url).then(r => r.json())
+const fetcher = url => fetch(url).then(r => r.json());
 
-export const SingleCategoryToggle = ({
-  category,
-  emailSettingsData
-}) => {
+export const SingleCategoryToggle = ({ category, emailSettingsData }) => {
   const { t } = useTranslation();
   const { control, getValues, setError, watch, handleSubmit } = useForm();
 
@@ -104,21 +122,21 @@ export const SingleCategoryToggle = ({
 
   const { emailSettingsHash } = useParams();
 
-  const onFormSuccess = data => {
-  };
+  const onFormSuccess = data => {};
 
   const onToggle = data => {
-    console.log("TBS", data, "onToggle")
-    
     const chageSubscribe = data[category] ? 'subscribe' : 'unsubscribe';
-    
-    fetch(`/api/email_settings/${emailSettingsHash}/${category}/${chageSubscribe}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+
+    fetch(
+      `/api/email_settings/${emailSettingsHash}/${category}/${chageSubscribe}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
       },
-      body: JSON.stringify({}),
-    })
+    );
   };
 
   useEffect(() => {
@@ -127,59 +145,71 @@ export const SingleCategoryToggle = ({
     return () => subscription.unsubscribe();
   }, [handleSubmit, watch]);
 
-  return <form>
-        <Controller
-          defaultValue={!emailSettingsData.unsubscribed_categories.includes(category)}
-          name={category}
-          control={control}
-          render={({
-            field: { onChange, onBlur, value, name, ref },
-            fieldState: { error },
-          }) => (
-            <Switch
-              id={category}
-              name={name}
-              inputRef={ref}
-              onCheckedChange={val => onChange({ target: { value: val } })}
-              onBlur={onBlur}
-              value={value}
-              defaultChecked={value}
-              error={error?.message}
-              label={t('mailing_lists.category_toggle', { category })}
-              labelInline={true}
-              required={false}
-            />
-          )}
-        />
-      </form>
-}
+  return (
+    <CategoryForm>
+      <Controller
+        defaultValue={
+          !emailSettingsData.unsubscribed_categories.includes(category)
+        }
+        name={category}
+        control={control}
+        render={({
+          field: { onChange, onBlur, value, name, ref },
+          fieldState: { error },
+        }) => (
+          <Switch
+            id={category}
+            name={name}
+            inputRef={ref}
+            onCheckedChange={val => onChange({ target: { value: val } })}
+            onBlur={onBlur}
+            value={value}
+            defaultChecked={value}
+            error={error?.message}
+            label={t(`mailing_lists.${category}_label_toggle`)}
+            labelInline
+            required={false}
+          />
+        )}
+      />
+    </CategoryForm>
+  );
+};
 
-export const DynamicPublicMailingListsSettings = ({
-  hideLabel,
-}: {
-  hideLabel?: boolean;
-}) => {
+export const DynamicPublicMailingListsSettings = () => {
   const { t } = useTranslation();
-  
-  const { emailSettingsHash } = useParams();
 
+  const { emailSettingsHash } = useParams();
+  const [saved, setSaved] = useState(false);
 
   const {
     data: emailSettingsData,
     error,
     isLoading,
-  } = useSWR(`/api/email_settings/${emailSettingsHash}/`, fetcher)
-  
-  console.log("TBS", emailSettingsData)
-
-
+  } = useSWR(`/api/email_settings/${emailSettingsHash}/`, fetcher);
 
   return (
     <MailingListsWrapper>
-       {isLoading && <div>Loading...</div>}
-       {!isLoading && emailSettingsData && emailSettingsData.categories.map((category) => {
-        return <SingleCategoryToggle category={category} emailSettingsData={emailSettingsData}/>
-       })}
+      {isLoading ? (
+        <Loading size={LoadingSizes.Medium} />
+      ) : (
+        emailSettingsData?.categories.map((category: string) => (
+          <SingleCategoryToggle
+            key={category}
+            category={category}
+            emailSettingsData={emailSettingsData}
+          />
+        ))
+      )}
+      <StatusMessage
+        $visible={saved || error?.message}
+        $type={saved ? MessageTypes.Success : MessageTypes.Error}
+      >
+        {saved ? t('mailing_lists.success_message') : error?.message}
+      </StatusMessage>
+      <Button disabled={false} loading={false} onClick={() => setSaved(true)}>
+        {t('mailing_lists.submit_btn')}
+      </Button>
     </MailingListsWrapper>
   );
 };
--- src/components/views/EmailPreferences.tsx
+++ src/components/views/EmailPreferences.tsx
@@ -1,45 +1,21 @@
 import {
-  Button,
-  MessageTypes,
-  StatusMessage,
-  TextTypes,
+  Card,
+  CardHeader,
+  CardSizes,
 } from '@a-little-world/little-world-design-system';
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
 
-import { updateEmailPreferences } from '../../api/emails.ts';
-import { onFormError, registerInput } from '../../helpers/form.ts';
-import ButtonsContainer from '../atoms/ButtonsContainer.jsx';
 import { DynamicPublicMailingListsSettings } from '../blocks/MailingLists/MailingLists.tsx';
-import { StyledCard, StyledForm, Title } from './SignUp.styles.jsx';
 
 const EmailPreferences = () => {
   const { t } = useTranslation();
 
   return (
-    <StyledCard>
-      <Title tag="h2" type={TextTypes.Heading4}>
-        {t('email_preferences.title')}
-      </Title>
+    <Card width={CardSizes.Medium}>
+      <CardHeader>{t('mailing_lists.title')}</CardHeader>
       <DynamicPublicMailingListsSettings />
-      {/** <StyledForm onSubmit={handleSubmit(onFormSubmit)}>
-        <StatusMessage
-          $visible={requestSuccessful || errors?.root?.serverError}
-          $type={requestSuccessful ? MessageTypes.Success : MessageTypes.Error}
-        >
-          {requestSuccessful
-            ? t('email_preferences.success_message')
-            : t(errors?.root?.serverError?.message)}
-        </StatusMessage>
-        <ButtonsContainer>
-          <Button type="submit" disabled={isSubmitting} loading={isSubmitting}>
-            {t('email_preferences.submit_btn')}
-          </Button>
-        </ButtonsContainer>
-      </StyledForm> **/}
-    </StyledCard>
+    </Card>
   );
 };
 
