1bdf3eb08b8352f711bd4fd886f40da8f6bd0e19
ConstantinHaag <constantin.haag@accenture.com>
2025-01-07 12:28:33 +0100

    attachement icon added

--- src/components/blocks/ChatCore/Chat.jsx
+++ src/components/blocks/ChatCore/Chat.jsx
@@ -1,5 +1,6 @@
 import {
   ArrowLeftIcon,
+  AttachementIcon,
   Button,
   ButtonSizes,
   ButtonVariations,
@@ -46,6 +47,7 @@ import { onFormError, registerInput } from '../../../helpers/form.ts';
 import useInfiniteScroll from '../../../hooks/useInfiniteScroll.tsx';
 import { MESSAGES_ROUTE, PROFILE_ROUTE, getAppRoute } from '../../../routes.ts';
 import {
+  AttachementButton,
   BackButton,
   ChatContainer,
   Message,
@@ -217,13 +219,27 @@ export const Chat = ({ chatId }) => {
           onSubmit={() => handleSubmit(onSendMessage)()}
           size={TextAreaSize.Xsmall}
         />
-        <AttachementIcon/>
-        <SendButton
+        <AttachementButton
           size={ButtonSizes.Large}
           type="submit"
           disabled={isSubmitting || activeChat?.is_unmatched}
           variation={ButtonVariations.Circle}
           backgroundColor={theme.color.gradient.orange10}
+        >
+          <AttachementIcon
+            label={t('attachement.upload_btn')}
+            labelId="attachement_icon"
+            color={theme.color.text.reversed}
+            width="20"
+            height="20"
+          />
+        </AttachementButton>
+        <SendButton
+          size={ButtonSizes.Large}
+          type="button"
+          variation={ButtonVariations.Circle}
+          backgroundColor={theme.color.gradient.orange10}
+          htmlfor = "FileInput"
         >
           <SendIcon
             label={t('chat.send_btn')}
