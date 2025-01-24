7946bb3d9f38ace80cbed3ee354ba0fe2d55b5e5
Tim Schupp <tim@timschupp.de>
2025-01-24 17:30:03 +0100

    fix missing slash prefix

--- src/api/livekit.ts
+++ src/api/livekit.ts
@@ -11,7 +11,7 @@ export const requestVideoAccessToken = async ({
   onError: (error: any) => void;
 }) => {
   try {
-    const result = await apiFetch(`api/livekit/authenticate`, {
+    const result = await apiFetch(`/api/livekit/authenticate`, {
       method: 'POST',
       useTagsOnly: true,
       body: { partner_id: partnerId },
@@ -26,33 +26,29 @@ export const submitCallFeedback = async ({
   liveSessionId,
   rating,
   review,
+  reviewId,
   onSuccess,
   onError,
 }: {
   liveSessionId?: string;
   rating: number;
   review?: string;
+  reviewId?: number;
   onSuccess: (result: any) => void;
   onError: (error: any) => void;
 }) => {
   try {
-    const response = await apiFetch(`api/livekit/review`, {
+    const response = await apiFetch(`/api/livekit/review`, {
       method: 'POST',
       body: {
         live_session_id: liveSessionId || null,
+        review_id: reviewId || null,
         rating: rating,
         review: review || '',
       },
     });
-
-    if (response.ok) {
-      const responseBody = await response.json();
-      onSuccess(responseBody);
-    } else {
-      const errorBody = await response.json();
-      onError(errorBody);
-    }
+    onSuccess(response);
   } catch (error) {
     onError(error);
   }
-}
+};
