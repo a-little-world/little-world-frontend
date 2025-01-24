6cdab72d676acfc8eda88cb2f9478f38c1da20f1
Tim Schupp <tim@timschupp.de>
2025-01-24 16:38:54 +0100

    ok use apiFetch(

--- src/api/livekit.ts
+++ src/api/livekit.ts
@@ -1,7 +1,5 @@
 import { apiFetch } from './helpers.ts';
 import Cookies from 'js-cookie';
-import { BACKEND_URL } from '../ENVIRONMENT.js';
-
 
 export const requestVideoAccessToken = async ({
   partnerId,
@@ -37,22 +35,14 @@ export const submitCallFeedback = async ({
   onSuccess: (result: any) => void;
   onError: (error: any) => void;
 }) => {
-  const url = `${BACKEND_URL}/api/livekit/review`;
-
-  const data = {
-    live_session_id: liveSessionId || null,
-    rating: rating,
-    review: review || '',
-  };
-
   try {
-    const response = await fetch(url, {
+    const response = await apiFetch(`api/livekit/review`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': Cookies.get('csrftoken') || '',
+      body: {
+        live_session_id: liveSessionId || null,
+        rating: rating,
+        review: review || '',
       },
-      body: JSON.stringify(data),
     });
 
     if (response.ok) {
