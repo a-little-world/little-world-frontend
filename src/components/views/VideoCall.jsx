b037ee53ca80f34855fdd50ab5c2f7ba13e6643b
ConstantinHaag <constantin.haag@accenture.com>
2025-02-04 13:44:24 +0100

    added useeffect changes to VideoCall.jsx

--- src/components/views/VideoCall.jsx
+++ src/components/views/VideoCall.jsx
@@ -50,6 +50,14 @@ function MyVideoConference({
     [{ source: Track.Source.Camera, withPlaceholder: true }],
     { onlySubscribed: true },
   );
+  const [currentParticipants, setCurrentParticipants] = useState(1);
+  const [otherUserDisconnected, setOtherUserDisconnected] = useState(false);
+
+  useEffect(() => {
+    if (tracks.length === 1 && currentParticipants > 1)
+      setOtherUserDisconnected(true);
+    setCurrentParticipants(tracks.length);
+  }, [tracks.length]);
 
   const { t } = useTranslation();
   const placeholders = {};
@@ -84,7 +92,14 @@ function MyVideoConference({
             imageType={partnerImageType}
             size="medium"
           />
-          <Text>{t('call.waiting_for_partner', { name: partnerName })}</Text>
+          <Text>
+            {t(
+              otherUserDisconnected
+                ? 'call.partner.disconnected'
+                : 'call.waiting_for_partner',
+              { name: partnerName },
+            )}
+          </Text>
         </WaitingTile>
       )}
     </Videos>
