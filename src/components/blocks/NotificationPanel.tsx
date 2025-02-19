4614868ecff2811af0f9ee84d0c0d192952ada94
JannisToelle <github.z8zet@simplelogin.com>
2025-02-19 14:36:44 +0100

    Notification styling

--- src/components/blocks/NotificationPanel.tsx
+++ src/components/blocks/NotificationPanel.tsx
@@ -1,15 +1,16 @@
-import { CalendarIcon, Text, TextTypes } from '@a-little-world/little-world-design-system';
+import {
+  CalendarIcon,
+  Text,
+  TextTypes,
+} from '@a-little-world/little-world-design-system';
 import { isEmpty } from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled, { css, useTheme } from 'styled-components';
 
-
-
 import { formatTimeDistance } from '../../helpers/date.ts';
 import { useSelector } from '../../hooks/index.ts';
 import ProfileImage from '../atoms/ProfileImage';
-
 
 const ProfileInfo = styled.div`
   display: flex;
@@ -72,6 +73,13 @@ const Notification = styled.li`
   }
 `;
 
+const NotificationTitle = styled(Text)`
+  max-width: 200px;
+  text-wrap: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 const Info = styled.div`
   display: flex;
   flex-direction: column;
@@ -83,7 +91,10 @@ const Time = styled(Text)`
 `;
 
 function NotificationPanel() {
-  const { t, i18n: {language} } = useTranslation();
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
   const theme = useTheme();
 
   const user = useSelector(state => state.userData.user);
@@ -123,8 +134,16 @@ function NotificationPanel() {
                 height="24"
               />
               <Info>
-                <Text type={TextTypes.Body5}>{title}</Text>
-                <Time type={TextTypes.Body6}>{formatTimeDistance(new Date(created_at), new Date(), language)}</Time>
+                <NotificationTitle type={TextTypes.Body5}>
+                  {title}
+                </NotificationTitle>
+                <Time type={TextTypes.Body6}>
+                  {formatTimeDistance(
+                    new Date(created_at),
+                    new Date(),
+                    language,
+                  )}
+                </Time>
               </Info>
             </Notification>
           ))
