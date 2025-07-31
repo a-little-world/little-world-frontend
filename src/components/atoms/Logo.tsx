149056f9e0a219efd99608f0d543f086f30c1aef
Tim Schupp <tim@timschupp.de>
2025-07-31 18:23:01 +0200

    make logo svg rendering compatible with both web and native

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
+import { environment } from '../../environment';
 import LogoImageSvg from '../../images/logo-image.svg';
 import LogoTextSvg from '../../images/logo-text.svg';
 import { getAppRoute } from '../../router/routes';
@@ -26,12 +27,13 @@ const LogoContainer = styled.div<{ $stacked?: boolean }>`
   `}
 `;
 
-const LogoImage = styled(LogoImageSvg)<{ $size: SizesType }>`
+// Styled components for both approaches
+const LogoImageStyled = styled.div<{ $size: SizesType }>`
   max-width: 100%;
   width: ${({ $size }) => ($size === LogoSizes.Small ? '30px' : '70px')};
 `;
 
-export const LogoText = styled(LogoTextSvg)<{ $size: SizesType }>`
+const LogoTextStyled = styled.div<{ $size: SizesType }>`
   max-width: 100%;
   width: ${({ $size }) => ($size === LogoSizes.Small ? '30px' : '80px')};
 `;
@@ -60,13 +62,63 @@ const Logo = ({
   displayText = true,
   stacked = true,
   size = LogoSizes.Medium,
-}: LogoProps) => (
-  <Wrapper asLink={asLink}>
-    <LogoContainer className={className} $stacked={stacked}>
-      {displayImage && <LogoImage $size={size} />}
-      {displayText && <LogoText $size={size} />}
-    </LogoContainer>
-  </Wrapper>
-);
+}: LogoProps) => {
+  const renderLogoImage = () => {
+    if (!displayImage) return null;
+
+    if (environment.isNative) {
+      // For native builds, use SVG as React component
+      return (
+        <LogoImageStyled $size={size}>
+          <LogoImageSvg />
+        </LogoImageStyled>
+      );
+    } else {
+      // For web builds, use SVG as src URL (webpack will handle the bundling)
+      return (
+        <LogoImageStyled $size={size}>
+          <img
+            src={LogoImageSvg}
+            alt="Little World Logo"
+            style={{ width: '100%', height: '100%' }}
+          />
+        </LogoImageStyled>
+      );
+    }
+  };
+
+  const renderLogoText = () => {
+    if (!displayText) return null;
+
+    if (environment.isNative) {
+      // For native builds, use SVG as React component
+      return (
+        <LogoTextStyled $size={size}>
+          <LogoTextSvg />
+        </LogoTextStyled>
+      );
+    } else {
+      // For web builds, use SVG as src URL (webpack will handle the bundling)
+      return (
+        <LogoTextStyled $size={size}>
+          <img
+            src={LogoTextSvg}
+            alt="Little World"
+            style={{ width: '100%', height: '100%' }}
+          />
+        </LogoTextStyled>
+      );
+    }
+  };
+
+  return (
+    <Wrapper asLink={asLink}>
+      <LogoContainer className={className} $stacked={stacked}>
+        {renderLogoImage()}
+        {renderLogoText()}
+      </LogoContainer>
+    </Wrapper>
+  );
+};
 
 export default Logo;
