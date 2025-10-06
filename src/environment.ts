fa9871535e61c8b570806da51ecea20760ee73cd
Tim Schupp <tim@timschupp.de>
2025-10-06 13:08:06 +0200

    default backend url must be empty

--- src/environment.ts
+++ src/environment.ts
@@ -17,7 +17,7 @@ export const environment: Environment = {
   production: true,
   defaultLoginName: 'benjamin.tim@gmx.de',
   defaultLoginPassword: 'Test123',
-  backendUrl: 'http://localhost:8000',
+  backendUrl: '',
   backendPath: '',
   coreWsScheme: 'wss://',
   coreWsPath: '/api/core/ws',
