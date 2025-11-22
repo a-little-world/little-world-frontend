223c61fa62e55cc92528d99c7fea24be9d135014
Tim Schupp <tim@timschupp.de>
2025-11-22 12:29:53 +0100

    add remaining random call apis

--- src/api/randomCalls.ts
+++ src/api/randomCalls.ts
@@ -1,17 +1,42 @@
 import { apiFetch } from './helpers';
 
-export const joinLobby = async ({ onSuccess }: { onSuccess: () => void }) => {
-  try {
-    // const response = await apiFetch('/api/random-calls/join-lobby/', {
-    //   method: 'POST',
-    // });
-    onSuccess();
-  } catch (error) {
-    console.error(error);
-  }
+export const joinLobby = async (lobbyName = 'default') => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/join`, {
+    method: 'POST',
+  });
 };
 
-export const submitRandomCallsFeedback = async () => {
+export const exitLobby = async (lobbyName = 'default') => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/exit`, {
+    method: 'POST',
+  });
+};
+
+export const getLobbyStatus = async (lobbyName = 'default') => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/status`, {
+    method: 'GET',
+  });
+};
+
+export const acceptMatch = async (lobbyName: string, matchUuid: string) => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/match/${matchUuid}/accept`, {
+    method: 'POST',
+  });
+};
+
+export const rejectMatch = async (lobbyName: string, matchUuid: string) => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/match/${matchUuid}/reject`, {
+    method: 'POST',
+  });
+};
+
+export const authenticateRoom = async (lobbyName: string, matchUuid: string) => {
+  return await apiFetch(`/api/random_calls/lobby/${lobbyName}/match/${matchUuid}/room_authenticate`, {
+    method: 'POST',
+  });
+};
+
+export const submitRandomCallsFeedback = async (feedback: any) => {
   try {
     const response = await apiFetch('/api/random-calls/submit-feedback/', {
       method: 'POST',
@@ -19,6 +44,7 @@ export const submitRandomCallsFeedback = async () => {
         feedback,
       },
     });
+    return response;
   } catch (error) {
     console.error(error);
   }
