2252adc2724eef64bc85b142b4a7c224f11d6fe1
JannisToelle <github.z8zet@simplelogin.com>
2026-05-05 13:16:19 +0200

    remove redundant websocket host environment

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,6 @@ export interface Environment {
   defaultLoginPassword: string;
   backendUrl: string;
   coreWsPath: string;
-  websocketHost: string;
   isNative: boolean;
   csrfBypassToken: string;
   allowNgrokRequests: boolean;
@@ -14,7 +13,6 @@ export const environment: Environment = {
   defaultLoginPassword: 'Test123',
   backendUrl: '',
   coreWsPath: '/api/core/ws',
-  websocketHost: '',
   isNative: false,
   csrfBypassToken: 'abc',
   allowNgrokRequests: false,
