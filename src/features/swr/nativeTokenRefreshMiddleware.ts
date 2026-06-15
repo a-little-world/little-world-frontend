779ecfa8d9a636d02c3fa3b13c666b047b97c022
JannisToelle <github.z8zet@simplelogin.com>
2026-06-15 14:38:43 +0200

    logout on missing access token

--- src/features/swr/nativeTokenRefreshMiddleware.ts
+++ src/features/swr/nativeTokenRefreshMiddleware.ts
@@ -1,5 +1,6 @@
-import type { Middleware } from 'swr';
+import { mutate, type Middleware } from 'swr';
 
+import { IS_AUTHENTICATED_ENDPOINT, USER_ENDPOINT } from '.';
 import { TokenStatus } from '../../api/types';
 import useNativeStore from '../stores/nativeStore';
 
@@ -14,19 +15,23 @@ const nativeTokenRefreshMiddleware: Middleware =
       try {
         return await fetcher(...args);
       } catch (error: any) {
-        const isTokenError =
-          error?.status === 401 || error?.code === 'token_not_valid';
-
-        if (!isTokenError) throw error;
-
         const {
-          setIsTokenRefreshing: setIstokenRefreshing,
+          getAccessToken,
+          setIsTokenRefreshing,
           refreshAccessToken,
           setTokenStatus,
         } = useNativeStore.getState();
+        const accessToken = await getAccessToken();
+
+        const isTokenExpiredError =
+          error?.status === 401 || error?.code === 'token_not_valid';
+        const isTokenMissingError = error?.status === 403 && !accessToken;
+        const isTokenError = isTokenExpiredError || isTokenMissingError;
+
+        if (!isTokenError) throw error;
 
         if (!tokenRefreshPromise) {
-          setIstokenRefreshing(true);
+          setIsTokenRefreshing(true);
           // infinite refresh loop prevented on native side -> if token expired/missing it does not perform any requests
           tokenRefreshPromise = refreshAccessToken()
             .then(status => {
@@ -34,7 +39,7 @@ const nativeTokenRefreshMiddleware: Middleware =
               return status;
             })
             .finally(() => {
-              setIstokenRefreshing(false);
+              setIsTokenRefreshing(false);
               tokenRefreshPromise = null;
             });
         }
@@ -43,6 +48,9 @@ const nativeTokenRefreshMiddleware: Middleware =
         if (tokenStatus === TokenStatus.VALID) {
           return fetcher(...args);
         }
+        // trigger logout
+        await mutate(IS_AUTHENTICATED_ENDPOINT, false, false);
+        await mutate(USER_ENDPOINT, null, false);
         throw new Error('Unauthenticated');
       }
     };
