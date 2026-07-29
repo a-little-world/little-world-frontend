84544a9cb68f2570a1d6e64c5fd5e14a5bfe198d
Simba14 <seanblundell@gmail.com>
2026-07-29 12:27:57 +0200

    update company hide label for dl-ukr

--- src/components/views/SignUp.tsx
+++ src/components/views/SignUp.tsx
@@ -46,7 +46,7 @@ const MTM_ENABLE_USER_TYPE_COOKIE = true;
 const SIGN_UP_COMPANY_SLUG_PREFIXES_HIDE_LABEL = [
   'campaign-',
   'self-organized-',
-  'dl-ukr',
+  'dl-',
 ] as const;
 
 function signUpCompanySlugHidesNameLabel(company: string): boolean {
