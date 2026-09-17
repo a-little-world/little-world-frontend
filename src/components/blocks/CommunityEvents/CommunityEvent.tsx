318bb8497782a0956763962286266625ec1bf864
JannisToelle <github.z8zet@simplelogin.com>
2026-09-17 21:08:34 +0200

    WIP

--- src/components/blocks/CommunityEvents/CommunityEvent.tsx
+++ src/components/blocks/CommunityEvents/CommunityEvent.tsx
@@ -459,9 +459,7 @@ function CommunityEvents() {
       ? buildRandomCallsEvent(
           upcomingLobbies ?? [],
           t('community_events.random_calls_title'),
-          t('community_events.random_calls_description', {
-            randomCallsLink: getAppAbsoluteRoute(RANDOM_CALLS_ROUTE),
-          }),
+          t('community_events.random_calls_description'),
           t('community_events.random_calls_cta'),
         )
       : null;
